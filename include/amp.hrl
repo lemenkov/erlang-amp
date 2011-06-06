d6f7d1181ea002e67914561f727d343e27233dcd
Peter Lemenkov <lemenkov@gmail.com>
2011-06-06 17:02:01 +0400

    Initial commit of the codec for AMP
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

---
+++ include/amp.hrl
@@ -0,0 +1,9 @@
+-define(ASK, '_ask').
+-define(ANSWER, '_answer').
+-define(COMMAND, '_command').
+-define(ERROR, '_error').
+-define(ERROR_CODE, '_error_code').
+-define(ERROR_DESCRIPTION, '_error_description').
+-define(UNKNOWN_ERROR_CODE, 'UNKNOWN').
+-define(UNHANDLED_ERROR_CODE, 'UNHANDLED').
+
--- src/amp.erl
+++ src/amp.erl
@@ -1,314 +1,124 @@
 %% Copyright (c) 2001-2010 Twisted Matrix Laboratories.
-%% See LICENSE for details.
+%% Copyright (c) 2011 Peter Lemenkov.
 %%
-%% AMP client.
+%% The MIT License
+%%
+%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%% of this software and associated documentation files (the "Software"), to deal
+%% in the Software without restriction, including without limitation the rights
+%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%% copies of the Software, and to permit persons to whom the Software is
+%% furnished to do so, subject to the following conditions:
+%%
+%% The above copyright notice and this permission notice shall be included in
+%% all copies or substantial portions of the Software.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+%% THE SOFTWARE.
+%%
+%% AMP codec.
 %%
 
 -module(amp).
 
--export([start_client/2, stop_client/1, client/2, sum/3, divide/3,
-         parse_int_prefixed/1, build_int_prefixed/1, parse_response/1,
-         make_command/2]).
+-include("../include/amp.hrl").
 
+-export([make_cmd/2]).
+-export([make_reply/2]).
+-export([parse_amp/1]).
+-export([get_type/1]).
+-export([get_command/1]).
 
-%%
-%% Start a new AMP client in a process.
-%%
-%% @param Host: the address of the AMP server.
-%% @type Host: C{string}
-%% @param Port: the port of the AMP server.
-%% @type Port: C{integer}
-%%
-%% @return: the PID of the client process.
-%% @rtype: C{pid}
-%%
-start_client(Host, Port) ->
-    spawn(amp, client, [Host, Port]).
+parse_amp(Binary) when is_binary(Binary) ->
+	parse_amp(Binary, []).
 
+parse_amp(Binary, DecodedAmps) ->
+	case parse_amp_single(Binary, []) of
+		{amp, DecodedAmp, <<>>} ->
+			{amp, DecodedAmps ++ [DecodedAmp], <<>>};
+		{amp, DecodedAmp, Rest} ->
+			parse_amp(Rest, DecodedAmps ++ [DecodedAmp]);
+		error ->
+			{amp, DecodedAmps, Binary}
+	end.
 
-%%
-%% Stop a previously started AMP client.
-%%
-%% @param AmpClientPid: the PID of the client process, returned by
-%%     C{start_client}.
-%% @type AmpClientPid: C{pid}
-%%
-stop_client(AmpClientPid) ->
-    AmpClientPid ! stop.
+parse_amp_single(<<0,0, Rest/binary>>, DecodedKVs) ->
+	{amp, DecodedKVs ++ [{eof}], Rest};
 
+parse_amp_single(<<LengthK:16, Data/binary>> = Data0, DecodedKVs) ->
+	try
+		<<Key:LengthK/binary, LengthV:16, R1/binary>> = Data,
+		<<Val:LengthV/binary, R2/binary>> = R1,
+		parse_amp_single(R2, DecodedKVs ++ [{list_to_atom(binary_to_list(Key)), Val}])
+	catch _:_ ->
+		error_logger:error_msg("bad_amp_raw: ~p, ~p~n", [Data0]),
+		error
+	end;
 
-%%
-%% Create a new client connection, and start a new AMP client loop. Don't call
-%% this, use start_client instead.
-%%
-%% @param Host: the address of the AMP server.
-%% @type Host: C{string}
-%% @param Port: the port of the AMP server.
-%% @type Port: C{integer}
-%%
-client(Host, Port) ->
-    case make_connect(Host, Port) of
-        {ok, Socket} ->
-            client_loop(Socket, 1);
-        {error, Reason} ->
-            io:format("Error in connection: ~p~n", [Reason]),
-            exit({error, Reason})
-    end.
+parse_amp_single(Rest, DecodedKVs) ->
+	error_logger:warning_msg("bad_amp_raw: ~p~n", [Rest]),
+	error.
 
+get_type([{?ASK, AmpTag} , {?COMMAND, CmdName} | RestPropList] = Amp) ->
+	?ASK;
+get_type([{?ERROR, AmpTag} , {?COMMAND, CmdName} | RestPropList] = Amp) ->
+	?ERROR;
+get_type([{?ANSWER, AmpTag} , {?COMMAND, CmdName} | RestPropList] = Amp) ->
+	?ANSWER.
 
-%%
-%% Main client loop for AMP.
-%%
-%% @param Socket: previously connected socket.
-%% @type Socket: C{socket}
-%% @param Counter: internal counter used to identify request.
-%% @type Counter: C{integer}
-%%
-client_loop(Socket, Counter) ->
-    receive
-        {command, CallPid, ArgList} ->
-            CounterAsList = integer_to_list(Counter),
-            Cmd = make_command(ArgList, CounterAsList),
-            case send_command(Socket, Cmd) of
-                {value, CounterAsList, Data} ->
-                    CallPid ! {value, Data};
-                {error, CounterAsList, Data} ->
-                    CallPid ! {error, Data};
-                {error, Reason} ->
-                    CallPid ! {error, Reason}
-            end,
-            client_loop(Socket, Counter + 1);
-        stop ->
-            gen_tcp:close(Socket)
-    end.
+get_command([{?ASK, AmpTag} , {?COMMAND, CmdName} | RestPropList] = Amp) ->
+	% This command name should already exists
+	{AmpTag, list_to_existing_atom(binary_to_list(CmdName)), RestPropList}.
 
+make_reply(Tag, Args) when is_list (Args), is_integer(Tag) ->
+	make_reply(<<Tag:32>>, Args);
+make_reply(Tag, Args) when is_list (Args), is_binary(Tag) ->
+	TagSize = size(Tag),
+	make_amp_raw(Args, <<7:16, "_answer", TagSize:16, Tag/binary>>).
+make_cmd(Command, Args) when is_list (Args) ->
+	AmpTag = make_tag(),
+	{SizeCommand, BinCommand} = to_binary(Command),
+	make_amp_raw(Args, <<4:16, "_ask", 4:16, AmpTag:32, 8:16, "_command", SizeCommand:16, BinCommand/binary>>).
 
-%%
-%% Create a connection to an AMP server.
-%%
-%% @param Host: the address of the AMP server.
-%% @type Host: C{string}
-%% @param Port: the port of the AMP server.
-%% @type Port: C{integer}
-%%
-%% @return: a tuple of ok, connected socket.
-%% @rtype: C{tuple}
-%%
-make_connect(Host, Port) ->
-    gen_tcp:connect(Host, Port, [binary, {packet, 0}]).
+make_amp_raw([], BinaryAmp) when is_binary(BinaryAmp) ->
+	<<BinaryAmp/binary, 0, 0>>;
 
+make_amp_raw([{Key, Value} | Rest], BinaryAmp) when is_binary(BinaryAmp) ->
+	make_amp_raw([Key, Value | Rest], BinaryAmp);
+make_amp_raw([Key, Value | Rest], BinaryAmp) when is_binary(BinaryAmp) ->
+	{BinaryKeySize, BinaryKey} = to_binary(Key),
+	{BinaryValueSize, BinaryValue} = to_binary(Value),
+	make_amp_raw(Rest, <<BinaryAmp/binary, BinaryKeySize:16, BinaryKey/binary, BinaryValueSize:16, BinaryValue/binary>>).
 
-%%
-%% Send a AMP command over a socket connection, and parse the result.
-%%
-%% @param Socket: a connected socket.
-%% @type Socket: C{socket}
-%% @param Cmd: a command previously build with C{make_command}.
-%% @type Cmd: C{binary}
-%%
-%% @return: data parsed from AMP result.
-%% @rtype: C{tuple}
-%%
-send_command(Socket, Cmd) ->
-    case gen_tcp:send(Socket, Cmd) of
-        ok ->
-            case wait_reply(Socket, 1000) of
-                {value, Reply} ->
-                    parse_response(Reply);
-                timeout ->
-                    timeout
-            end;
-        {error, Reason} ->
-            {error, Reason}
-    end.
+make_tag() ->
+	{A1,A2,A3} = now(),
+	random:seed(A1, A2, A3),
+	% 4294967296 == INT_MAX
+	random:uniform(4294967296).
 
+to_binary(Value) when is_atom (Value) ->
+	to_binary(list_to_binary(atom_to_list(Value)));
+to_binary(Value) when is_list (Value) ->
+	to_binary(list_to_binary(Value));
+to_binary(Value) when is_integer (Value) ->
+	to_binary(list_to_binary(integer_to_list(Value)));
+to_binary(Value) when is_float (Value) ->
+	to_binary(list_to_binary(float_to_list(Value)));
+to_binary(Value) when is_binary (Value) ->
+	{size(Value), Value}.
 
-%%
-%% Parse a response to an AMP command.
-%%
-%% @param BinData: raw result for the AMP command.
-%% @type BinData: C{binary}
-%%
-%% @return: parsed result of the command.
-%% @rtype: C{tuple}
-%%
-parse_response(BinData) ->
-    Data = parse_int_prefixed(BinData),
-    case Data of
-        ["_answer", AnswerID | Rest] ->
-            {value, AnswerID, Rest};
-        ["_error", AnswerID | Rest] ->
-            {error, AnswerID, Rest};
-        _ ->
-            {error, Data}
-    end.
+%% Tests
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
 
+%amp_test() ->
+%	AmpBin = amp:make_amp("hello", ["foo", "bar"]),
+%	AmpParsed = amp:parse_amp(AmpBin),
+%	?assertEqual(AmpParsed, amp:parse_amp(AmpBin)).
 
-%%
-%% Build body of an AMP command.
-%%
-%% @param ArgList: name of the command, and its arguments.
-%% @type ArgList: C{list}
-%% @param Tag: AMP tag used to identify answer.
-%% @type Tag: C{string}
-%%
-make_command(ArgList, Tag) ->
-    build_int_prefixed(["_ask", Tag, "_command" | ArgList]).
-
-
-%%
-%% Parse binary data as int16-prefixed data NULL terminated.
-%%
-%% @param BinData: data to parse.
-%% @type BinData: C{binary}
-%%
-%% @return: tokens read from binary data.
-%% @rtype: C{list}
-%%
-parse_int_prefixed(BinData) ->
-    parse_int_prefixed(BinData, []).
-
-
-%%
-%% Parse binary data as int16-prefixed data NULL terminated.
-%%
-%% @param BinData: data to parse.
-%% @type BinData: C{binary}
-%% @param List: used to hold temporary result during recursion.
-%% @type List: C{list}
-%%
-%% @return: tokens read from binary data.
-%% @rtype: C{list}
-%%
-parse_int_prefixed(BinData, List) ->
-    case BinData of
-        <<0, 0>> ->
-            List;
-        <<Prefix:16, RemainData/binary>> ->
-            BinaryLength = length(binary_to_list(RemainData)),
-            CheckLength = BinaryLength < Prefix,
-            case CheckLength of
-                false ->
-                    {ReadData, Remain} = split_binary(RemainData, Prefix),
-                    NewList = lists:append(List, [binary_to_list(ReadData)]),
-                    parse_int_prefixed(Remain, NewList);
-                true ->
-                    error
-            end;
-        _ ->
-            error
-    end.
-
-
-%%
-%% Build int16-prefixed binary data NULL terminated.
-%%
-%% @param ListData: tokens of the data.
-%% @type ListData: C{list}
-%%
-%% @return: int16-prefixed raw data.
-%% @rtype: C{binary}
-%%
-build_int_prefixed(ListData) ->
-    BuiltData = build_int_prefixed(ListData, <<>>),
-    % Add final NULL values
-    <<BuiltData/binary, 0, 0>>.
-
-
-%%
-%% Inner function to build int16-prefixed binary data: stop point for empty
-%% list.
-%%
-build_int_prefixed([], BinaryData) ->
-    BinaryData;
-
-%%
-%% Inner function to build int16-prefixed binary data.
-%%
-build_int_prefixed([Head | RemainData], BinaryData) when is_list(Head)->
-    LengthHead = length(Head),
-    BinData = list_to_binary(Head),
-    build_int_prefixed(RemainData, <<BinaryData/binary, LengthHead:16,
-                       BinData/binary>>);
-
-build_int_prefixed([IntHead | RemainData], BinaryData) when is_integer(IntHead)->
-    build_int_prefixed([integer_to_list(IntHead) | RemainData], BinaryData);
-
-build_int_prefixed([FloatHead | RemainData], BinaryData) when is_float(FloatHead) ->
-    build_int_prefixed([float_to_list(FloatHead) | RemainData], BinaryData);
-
-build_int_prefixed([true | RemainData], BinaryData) ->
-    build_int_prefixed(["True" | RemainData], BinaryData);
-
-build_int_prefixed([false | RemainData], BinaryData) ->
-    build_int_prefixed(["False" | RemainData], BinaryData).
-
-
-%%
-%% Helper to get data from a socket.
-%%
-wait_reply(_, Timeout) ->
-    receive
-        {tcp, _, Reply} ->
-            {value, Reply}
-        after Timeout ->
-            timeout
-    end.
-
-
-process_command(AmpClientPid, ArgList) ->
-    case is_process_alive(AmpClientPid) of
-        false ->
-            {error, "Invalid process handle"};
-        true ->
-             AmpClientPid ! {command, self(), ArgList}
-    end.
-
-
-%%
-%% Test function to call the AMP sum command.
-%%
-%% Example::
-%%      amp:sum(Pid, 12, 34).
-sum(AmpClientPid, A, B) ->
-    case process_command(AmpClientPid, ["Sum",
-                                        "a", A,
-                                        "b", B]) of
-        {error, Reason} ->
-            {error, Reason};
-        _ ->
-            receive
-                {value, Data} ->
-                    case Data of
-                        ["total", Answer] ->
-                            list_to_integer(Answer)
-                    end;
-                {error, Data} ->
-                    {error, Data}
-            end
-    end.
-
-
-%%
-%% Test function to call the AMP divide command.
-%%
-%% Example::
-%%      amp:divide(Pid, 12, 4).
-divide(AmpClientPid, A, B) ->
-    case process_command(AmpClientPid, ["Divide",
-                                        "numerator", A,
-                                        "denominator", B]) of
-        {error, Reason} ->
-            {error, Reason};
-        _ ->
-        receive
-            {value, Data} ->
-                case Data of
-                    ["result", Answer] ->
-                        list_to_float(Answer)
-                end;
-            {error, Data} ->
-                {error, Data}
-        end
-    end.
-
+-endif.
